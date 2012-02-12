9a3f673a54b4941278404d94f3353b623f154e8f
Phil Dokas <phil@jetless.org>
2012-02-12 13:14:21 -0800

    Make use of bootstrap on homepage

--- templates/homepage.php
+++ templates/homepage.php
@@ -1,17 +1,19 @@
 <?php include('head.php'); ?>
 
-<?php if (count($people)): ?>
-<ol>
 <?php foreach ($people as $i => $p): ?>
-	<li>
+<?php if ($i % 6): ?>
+<div class="row">
+<?php endif ?>
+	<div class="span2">
 		<h2><?php echo $p['displayname'] ?></h2>
 		
 		<a href='<?php echo $p['url'] ?>'>
 			<img src='<?php echo $p['buddyicon'] ?>'>
 		</a>
-	</li>
-<?php endforeach ?>
-</ol>
+	</div>
+<?php if ($i % 6): ?>
+</div>
 <?php endif ?>
+<?php endforeach ?>
 
 <?php include('foot.php'); ?>
