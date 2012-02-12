13c8f3068ede7cbb3e76491d050d456ec0acce25
Phil Dokas <phil@jetless.org>
2012-02-12 13:23:41 -0800

    Design++

--- templates/signin.php
+++ templates/signin.php
@@ -1,7 +1,9 @@
 <?php include('head.php'); ?>
 
 <?php if ($nextStep): ?>
+<div class='span12'>
 <a href='<?php echo $nextStep; ?>'>Go west, young man</a>
+</div>
 <?php endif ?>
 
 <?php include('foot.php'); ?>
