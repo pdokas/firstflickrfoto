4e323e26b9d7805b7722c66719c0e1e8b3aba95c
Phil Dokas <phil@jetless.org>
2012-02-26 01:10:48 -0800

    Styling improvement

--- templates/homepage.php
+++ templates/homepage.php
@@ -7,14 +7,10 @@
 <div class='row'>
 <?php endif ?>
 	<div class='person span2' data-photo-url='<?php echo "{$p['url']}{$p['first_photo']['id']}/" ?>'>
-		<div class='photo'>
-			<img src='<?php echo "http://farm{$p['first_photo']['farm']}.staticflickr.com/{$p['first_photo']['server']}/{$p['first_photo']['id']}_{$p['first_photo']['secret']}_m.jpg" ?>'>
-		</div>
+		<div class='photo' style="background-image: url(<?php echo "http://farm{$p['first_photo']['farm']}.staticflickr.com/{$p['first_photo']['server']}/{$p['first_photo']['id']}_{$p['first_photo']['secret']}.jpg" ?>);"></div>
 		<div class='nametag'>
-			<a href='<?php echo $p['url'] ?>'>
-				<img src='<?php echo $p['buddyicon'] ?>'>
-				<h6><?php echo $p['name'] ?></h6>
-			</a>
+			<img src='<?php echo $p['buddyicon'] ?>'>
+			<h6><?php echo $p['name'] ?></h6>
 		</div>
 	</div>
 <?php if ($i % 6 === 5): ?>
